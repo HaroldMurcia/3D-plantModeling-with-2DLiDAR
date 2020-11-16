2721817467050852b20932c3fd62b3d0096a077b
haroldmurcia <harold.murcia@unibague.edu.co>
2020-11-16 16:18:38 -0500

    V1.0

--- build/plataforma/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hmurcia/repos/3D-plantModeling-with-2DLiDAR/src/plataforma
-# Build directory: /home/hmurcia/repos/3D-plantModeling-with-2DLiDAR/build/plataforma
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/plataforma/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;gazebo_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "plataforma"
-PROJECT_SPACE_DIR = "/home/hmurcia/repos/3D-plantModeling-with-2DLiDAR/install"
-PROJECT_VERSION = "0.0.0"
